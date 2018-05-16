2057636c0d14d5b8bef34e1282e6de15c171c742
TheRealDan <thedanielhailwood@gmail.com>
2018-05-16 20:08:07 +1200

    New controller

---
+++ src/me/therealdan/dansengine/window/input/controller/ArrowKeysController.java
@@ -0,0 +1,12 @@
+package me.therealdan.dansengine.window.input.controller;
+
+public interface ArrowKeysController {
+
+    void up();
+
+    void down();
+
+    void left();
+
+    void right();
+}
---
+++ src/me/therealdan/dansengine/window/input/controller/WASDController.java
@@ -0,0 +1,12 @@
+package me.therealdan.dansengine.window.input.controller;
+
+public interface WASDController {
+
+    void up();
+
+    void down();
+
+    void left();
+
+    void right();
+}
