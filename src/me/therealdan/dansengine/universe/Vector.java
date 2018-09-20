88e942d662bc449c9761b4e1e3be7d4e8bc6439f
TheRealDan <thedanielhailwood@gmail.com>
2018-09-20 22:43:02 +1200

    Added setX setY setZ

--- src/me/therealdan/dansengine/universe/Vector.java
+++ src/me/therealdan/dansengine/universe/Vector.java
@@ -39,6 +39,18 @@ public class Vector {
         this.z = z;
     }
 
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+
     public void add(double x, double y) {
         this.x += x;
         this.y += y;
